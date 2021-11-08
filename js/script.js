c39c249609bf1faa1b5703a354a559ad443b8701
Nastya333999 <paradisejustforyou@gmail.com>
2021-11-08 11:03:16 +0200

    end

--- js/script.js
+++ js/script.js
@@ -1,21 +1,22 @@
+// это абстракция т.к. описывает в общем все автомобили
 class AbstractCars {
     getSpeed(){
-        'moves faster than a turtle'
+        return 'moves faster than a turtle'
     }
 
     getTime(){
-        'moves at the speed of light'
+        return 'moves at the speed of light'
     }
-    
 }
 
+// ауди наследует свойства от абстрактного класса
 class Audi extends AbstractCars {
     constructor(time,distance) {
         super()
         this._time = time;
         this._distance = distance;
     }
-
+// это инкапсуляция так как показано состояние (тут это скорость) определенного автомобиля
     getSpeed () {
         return this._distance / this._time; 
     }
@@ -46,13 +47,58 @@ class Reno extends AbstractCars {
     
 }
 
+class Bus extends AbstractCars {
+    constructor(time, distance){
+        super()
+        this._time = time; 
+        this._distance = distance;
+    }
+
+    getSpeed () {
+        return this._distance / this._time; 
+    }
+}
+
+// полиморфизм - показан в том, что у каждой машины есть скорость и мы сможем сложить и узнать обущю скорость
+class Speeds {
+    constructor() {
+        this._cars = [];
+    }
+    addCars(cars) {
+        if (!cars){
+            return cars;
+        }
+        this ._cars.push(cars);
+    }
+
+    speedss(){
+        const result = this._cars.reduce((acc, i) => {
+
+            return acc + i.getSpeed()
+        }, 0)
+            return result
+    }
+}
+
 
 const myOldCar = new Audi(2, 180); 
 console.log(myOldCar.getSpeed());
 
 const myFirstCar = new Reno(2, 100);
 console.log(myFirstCar.getSpeed());
-console.log(myFirstCar.getTime()); // undefined???
+// показывает как рено наследуюет от абстрактного класса getTime 
+console.log(myFirstCar.getTime());
 
 const myCar = new Mustang (2,200); 
 console.log(myCar.getSpeed());
+
+const bus = new Bus (4, 80); 
+console.log(bus.getSpeed()); 
+
+const calc = new Speeds(); 
+calc.addCars(myOldCar);
+calc.addCars(myFirstCar);
+calc.addCars(myCar);
+calc.addCars(bus);
+
+console.log(calc.speedss())
